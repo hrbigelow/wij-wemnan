2201587821618c6929e9b9d1776bf454ea3150f8
Henry Bigelow <hrbigelow@gmail.com>
2014-01-26 22:49:19 -0800

    factoring out generic code from scatter plot and into webgl_utils

---
+++ js/app/offscreen_rendering.js
@@ -0,0 +1,53 @@
+define([
+], function() {
+    
+    // a renderbuffer for representing each vertex as a single pixel
+    // creates a framebuffer, renderbuffer, and attaches the latter
+    function VertexPicker(gl) {
+        this.gl = gl;
+        this.npoints = null;
+        this.width = this.default_width;
+        this.height = Math.ceil(this.npoints / this.width);
+        this.jsbuf = null;
+
+        this.fb = this.gl.createFramebuffer();
+        this.rb = this.gl.createRenderbuffer();
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.fb);
+        this.gl.framebufferRenderbuffer(this.gl.FRAMEBUFFER, 
+                                        this.gl.COLOR_ATTACHMENT0, 
+                                        this.gl.RENDERBUFFER,
+                                        this.rb);
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+
+    }
+
+    VertexPicker.prototype = {
+        default_width: 1000,
+
+        resize: function(npoints, new_arraybuf) {
+            this.npoints = npoints;
+            this.height = Math.ceil(this.npoints / this.width);
+            this.gl.bindRenderbuffer(this.gl.RENDERBUFFER, this.rb);
+            this.gl.renderbufferStorage(this.gl.RENDERBUFFER, this.gl.RGBA4, 
+                                        this.width, this.height);
+            this.jsbuf = new Float32Array(new_arraybuf, this.npoints);
+        },
+        destroy: function() {
+            this.gl.deleteRenderbuffer(this.rb);
+            this.gl.deleteFramebuffer(this.fb);
+        },
+        // reads pixels from gl framebuffer into jsbuf
+        read: function() {
+            this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.fb);
+            this.gl.readPixels(0, 0, this.width, this.height, 
+                               this.gl.RGBA, this.gl.UNSIGNED_BYTE,
+                               this.jsbuf);
+        }
+
+    };
+        
+    return {
+        VertexPicker: VertexPicker
+    };
+
+});
